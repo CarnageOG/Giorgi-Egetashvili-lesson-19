7da0cda11cda0b3c0b76f8e89a936d2440bd158d
CarnageOG <egetashviligiorgi9@gmail.com>
2025-02-28 19:08:33 +0400

    made first and second homeworks

---
+++ JavaScript/app.js
@@ -0,0 +1,17 @@
+//.1////////////////////////////////////////////////////////////////
+function deleteButton() {
+            const button = document.getElementById("delete-me");
+            button.remove();
+        }
+
+
+
+//.2////////////////////////////////////////////////////////////////
+const img = document.createElement('img');
+img.src = 'https://fastly.picsum.photos/id/180/2000/1600.jpg?hmac=dr3QTXUHbgPDvKgE9s2UjynoVFsZdxoLI3cxK8YZd9A';
+img.alt = 'Random Image';
+document.body.appendChild(img);
+
+
+
+//.3////////////////////////////////////////////////////////////////
